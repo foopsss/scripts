2cb456c5fc7f347e99cada42e00a004c9e1d6e64
Lucas <libenchat@hotmail.com>
2024-09-14 02:36:13 -0300

    Modificación de GitBackups.ps1
    Simplifiqué un poco el script, deshaciéndome de los arreglos adicionales para separar los repositorios que tienen múltiples ramas de los que no.
    
    Esto va a causar que ahora muchos de los repositorios estén doblemente comprimidos, pero no me parece el fin del mundo.
    
    También cambié ligeramente el nombre de la variable "branch_name" para que se distinga de cuando estoy tratando la propiedad "name" de un elemento del arreglo "branches".

--- Backups/PowerShell/GitBackups.ps1
+++ Backups/PowerShell/GitBackups.ps1
@@ -1,9 +1,7 @@
 Import-Module -Name $pwd/CommonResources.psm1
 
-$main_only = @("foopsss", "gentoo-configs", "gentoo-overlay", "natural-gentoo-remastered", "specfiles")
-$other_repos = @("algoritmos", "algoritmos-rs", "dotfiles", "scripts")
+$repos = @("algoritmos", "algoritmos-rs", "dotfiles", "foopsss", "gentoo-configs", "gentoo-overlay", "natural-gentoo-remastered", "scripts", "specfiles")
 $branches = @()
-$all_repos = @()
 
 $url = "www.github.com"
 $is_reachable = Test-Connection $url -Count 1 -Quiet
@@ -15,11 +13,7 @@ if (!$is_reachable) {
 	Write-Host "Procediendo a descargar los repositorios..." -ForegroundColor Gray 
 }
 
-foreach ($repo in $main_only) {
-	Invoke-WebRequest -Uri "https://github.com/foopsss/$repo/archive/refs/heads/main.zip" -OutFile "$repo.zip"
-}
-
-foreach($repo in $other_repos) {
+foreach($repo in $repos) {
 	# Averiguo el nombre de las ramas disponibles con una solicitud GET.
 	# Utilizo la API de GitHub.
 	$branches = Invoke-RestMethod -Uri "https://api.github.com/repos/foopsss/$repo/branches" -Method GET
@@ -27,9 +21,9 @@ foreach($repo in $other_repos) {
 	# Guardo en una variable el nombre de cada rama y la utilizo para descargarlas.
 	# Luego modifico el nombre en el arreglo $branches para darle el nombre de su archivo correspondiente.
 	foreach($branch in $branches) {
-		$branch_name = $branch.name
+		$branch_name_var = $branch.name
 		$branch.name = $branch.name | ForEach-Object {"$repo-$_.zip"}
-		Invoke-WebRequest -Uri "https://github.com/foopsss/$repo/archive/refs/heads/$branch_name.zip" -OutFile "$repo-$branch_name.zip"
+		Invoke-WebRequest -Uri "https://github.com/foopsss/$repo/archive/refs/heads/$branch_name_var.zip" -OutFile "$repo-$branch_name_var.zip"
 	}
 	
 	# Comprimo en un solo archivo las multiples ramas de un repositorio.
@@ -37,25 +31,23 @@ foreach($repo in $other_repos) {
 	Remove-Item -LiteralPath $branches.name
 }
 
-# Modifico los arreglos de entrada con los repositorios.
-# La idea es añadirles un sufijo, la extension ".zip".
+# Modifico el arreglo de entrada con los repositorios.
+# La idea es añadirle la extension ".zip" a los elementos.
 # Luego comprimo todos los archivos indicados en uno solo.
-$main_only = $main_only | ForEach-Object {"$_.zip"}
-$other_repos = $other_repos | ForEach-Object {"$_.zip"}
-$all_repos = $main_only + $other_repos
+$repos = $repos | ForEach-Object {"$_.zip"}
 
 if (is_windows) {
-	Compress-Archive -LiteralPath $all_repos -DestinationPath "$onedrive_win\GitHub.zip" -Force
+	Compress-Archive -LiteralPath $repos -DestinationPath "$onedrive_win\GitHub.zip" -Force
 	Copy-Item "$onedrive_win\GitHub.zip" -Destination "$zips_win\GitHub.zip" -Force
 	Test-Existence -arch1 "$onedrive_win\GitHub.zip" -arch2 "$zips_win\GitHub.zip"
 } else {
 	# La funcion "is_windows" solo devuelve "Unix" si no uso Windows.
 	# Sin embargo, me es suficiente para el uso que le voy a dar en Linux.
-	Compress-Archive -LiteralPath $all_repos -DestinationPath "$onedrive_linux/GitHub.zip" -Force
+	Compress-Archive -LiteralPath $repos -DestinationPath "$onedrive_linux/GitHub.zip" -Force
 	Copy-Item "$onedrive_linux/GitHub.zip" -Destination "$zips_linux/GitHub.zip" -Force
 	Test-Existence -arch1 "$onedrive_linux/GitHub.zip" -arch2 "$zips_linux/GitHub.zip"
 }
 
-Remove-Item -LiteralPath $all_repos
+Remove-Item -LiteralPath $repos
 
 Read-Key
