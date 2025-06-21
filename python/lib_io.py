ab380fbcf128ff890cb26abf78898cfca0b4101e
Lucas Ivan Benchat Parra <libenchat@hotmail.com>
2025-06-20 23:42:21 -0300

    Mejoras en las librerías de Python
    * Adición de las funciones run_program y pipe_programs en lib_misc.
    * Comentarios para la mayoría de las funciones con explicaciones detalladas de su funcionamiento.
    * Mejor lógica para lidiar con errores en bg_colour y get_choice.

--- python/lib_io.py
+++ python/lib_io.py
@@ -6,6 +6,10 @@ def clear_screen():
     subprocess.run(["clear"])
 
 def draw_line(length, symbol="-"):
+    # length permite indicar la longitud deseada para
+    # las líneas, mientras que symbol permite indicar
+    # el símbolo a utilizar, aunque por defecto se
+    # imprimen líneas con el carácter "-".
     for i in range(length - 1):
         print(symbol, end='')
     print(symbol)
@@ -15,12 +19,17 @@ def bg_colour(colour, text):
     # El rojo se usa para mensajes de error.
     # El verde para mensajes de éxito o confirmación.
     # El azul para mostrar otros mensajes por pantalla.
+    #
+    # Si no especifica ningún color válido no
+    # se colorea el fondo del texto y solo se
+    # lo imprime en negritas.
     match colour:
-        case "red": colour = 41
-        case "green": colour = 42
-        case "blue": colour = 44
+        case "red": background = 41
+        case "green": background = 42
+        case "blue": background = 44
+        case _: background = 49
 
-    print(f"\033[1;37;{colour}m{text}\033[0m")
+    print(f"\033[1;37;{background}m{text}\033[0m")
 
 def get_choice(low_lim, upp_lim):
     # low_lim hace referencia a la cota inferior del rango
@@ -29,14 +38,25 @@ def get_choice(low_lim, upp_lim):
     # de dicho rango.
     while True:
         choice_str = input("Ingrese su elección: ")
-        choice = int(choice_str)
 
-        if choice < low_lim or choice > upp_lim:
-            # Si el usuario introduce un valor por fuera del rango
-            # aceptado, hay que imprimir un mensaje de error y luego
-            # solicitarle una elección correcta.
-            bg_colour("red", "¡Introduzca un número válido!")
+        # Si el usuario no introduce nada y presiona ENTER,
+        # o si introduce caracteres que no sean números, hay
+        # que imprimir un mensaje de error y solicitar una
+        # elección correcta.
+        if choice_str == "":
+            bg_colour("red", "¡Introduzca una elección!")
+            continue
+        elif not choice_str.isdigit():
+            bg_colour("red", "¡Solo se pueden introducir números!")
             continue
         else:
-            # De lo contrario, se puede seguir con el programa.
-            return(choice)
+            choice = int(choice_str)
+            if choice < low_lim or choice > upp_lim:
+                # Si el usuario introduce un valor por fuera del
+                # rango aceptado, hay que imprimir un mensaje de
+                # error y luego solicitarle una elección correcta.
+                bg_colour("red", "¡Introduzca un número válido!")
+                continue
+            else:
+                # De lo contrario, se puede seguir con el programa.
+                return(choice)
