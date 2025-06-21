8792d813cb99f422b362ee3897db011e67b0df10
Lucas Ivan Benchat Parra <libenchat@hotmail.com>
2025-06-20 23:42:21 -0300

    Mejoras en las librerías de Python
    * Adición de las funciones run_program y pipe_programs en lib_misc.
    * Comentarios para la mayoría de las funciones con explicaciones detalladas de su funcionamiento.
    * Mejor lógica para lidiar con errores en bg_colour y get_choice.

--- python/lib_misc.py
+++ python/lib_misc.py
@@ -4,23 +4,78 @@ import subprocess
 
 from lib_io import bg_colour
 
+def run_program(command, check_return=True, use_shell=False):
+    # run_program es un wrapper de subprocess.run para
+    # ejecutar programas externos. Permite deshabilitar
+    # el control de errores de ser necesario, pero por
+    # defecto se controla el código de salida que
+    # devuelven los programas llamados.
+    #
+    # run_program también permite utilizar el intérprete
+    # de consola del sistema para realizar operaciones,
+    # pero esto solo se puede hacer con el control de
+    # errores activado.
+    if check_return:
+        try:
+            if use_shell:
+                subprocess.run(command, check=True, shell=True)
+            else:
+                subprocess.run(command, check=True)
+        except subprocess.CalledProcessError as e:
+            bg_colour("red", "Error de ejecución del programa.")
+            bg_colour("red", f"Código de salida: {e.returncode}")
+    else:
+        if use_shell:
+            raise ValueError("No se puede usar check=False con shell=True.")
+        else:
+            subprocess.run(command, check = False)
+
 def run_with_pkexec(command, mult_op=False):
-    # "mult_op" significa "multiples operaciones"
-    # y se usa para indicar si se van a realizar
-    # múltiples operaciones con pkexec o no, en
-    # cuyo caso es necesario pasarle todos los
-    # comandos en una cadena al intérprete de
-    # Bash y ejecutar eso con pkexec.
+    # run_with_pkexec es un wrapper de subprocess.run
+    # que sirve para ejecutar programas externos con
+    # permisos elevados, permitiendo realizar múltiples
+    # operaciones al mismo tiempo de ser requerido a
+    # través de una llamada al intérprete de Bash,
+    # pasándole todos los comandos en cadena.
     if mult_op:
         pkexec_var = ["pkexec", "bash", "-c"]
     else:
         pkexec_var = ["pkexec"]
 
     try:
-        result = subprocess.run(pkexec_var + command, check = True)
-    except subprocess.CalledProcessError as error:
-        bg_colour("red", "La operación ha fallado.")
-        bg_colour("red", f"Código de error: {error.returncode}")
+        subprocess.run(pkexec_var + command, check=True)
+    except subprocess.CalledProcessError as e:
+        bg_colour("red", "Error de ejecución del programa.")
+        bg_colour("red", f"Código de salida: {e.returncode}")
+
+def pipe_programs(proc1, proc2):
+    # pipe_programs es un wrapper de subprocess.Popen
+    # que sirve para simular el uso de pipelines como
+    # las que se utilizan en los intérpretes de consola.
+    # Solo admite la llamada a dos programas.
+    #
+    # Recibe como entrada dos listas con los programas
+    # a ejecutar.
+    try:
+        proc1_out = subprocess.Popen(proc1, stdout=subprocess.PIPE)
+
+        try:
+            proc2_out = subprocess.Popen(proc2, stdin=proc1_out.stdout,
+                                         stdout=subprocess.PIPE, text=True)
+            proc1_out.stdout.close()
+            return proc2_out.communicate()[0]
+        # Manejo de errores de la segunda función llamada.
+        except OSError as os_err_two:
+            # Manejo de errores relacionados con el SO como "archivo
+            # no encontrado", "permiso denegado", etc.
+            bg_colour("red", "Error del SO durante la ejecución del segundo programa.")
+            print(f"{os_err_two}")
+            return None
+    # Manejo de errores de la primera función llamada.
+    except OSError as os_err_one:
+        bg_colour("red", "Error del SO durante la ejecución del primer programa.")
+        print(f"{os_err_one}")
+        return None
 
 def press_enter():
     print("")
