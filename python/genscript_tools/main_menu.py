27e3c8f139f9f3ca9ef1507e819528444047a3d3
Lucas Ivan Benchat Parra <libenchat@hotmail.com>
2025-10-12 19:41:09 -0300

    [python] Port to execute_with_interrupt_handler() from modules/program_tools.py
    Ported both genscript.py and run_black.py to the execute_with_interrupt_handler() function from program_tools.py to guarantee a smooth exit if the execution is manually interrupted.
    
    Also, moved all the main menu logic from genscript.py to a module of its own inside "genscript_tools", alongside the functions that were previously available in the "system_maintenance" module.

--- python/genscript_tools/main_menu.py
+++ python/genscript_tools/main_menu.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 # Paquetes usados en este módulo:
-# app-admin/eselect - provee "eselect" y sus módulos.
+# app-portage/gentoolkit  - provee "eclean".
+# app-admin/eclean-kernel - provee "eclean-kernel".
+# sys-apps/portage        - provee "dispatch-conf".
+# sys-apps/coreutils      - provee "cat" y "wc".
+# app-admin/eselect       - provee "eselect" y sus módulos.
 
 import os
 import shutil
@@ -16,6 +20,11 @@ from modules.subprocess_utils import (
     pipe_commands,
     run_command,
 )
+
+from genscript_tools.package_management import PACKAGE_MANAGEMENT_MENU_DATA
+from genscript_tools.snapshots import SNAPSHOT_MANAGEMENT_MENU_DATA
+from genscript_tools.updates import UPDATES_MENU_DATA
+from genscript_tools.use_flags import USE_FLAGS_MENU_DATA
 
 
 def clean_thumbnails():
@@ -124,3 +133,62 @@ def read_news():
         check_return=True,
         use_shell=True,
     )
+
+
+MAIN_MENU_DATA = {
+    "dict_name": "MAIN_MENU_DATA",
+    "title": "¡Bienvenido a la herramienta de administración del sistema!",
+    "options": [
+        {"name": "APARTADOS ADICIONALES"},
+        {
+            "name": "Menú de opciones de actualización.",
+            "action": UPDATES_MENU_DATA,
+        },
+        {
+            "name": "Menú de manejo de paquetes y repositorios del sistema.",
+            "action": PACKAGE_MANAGEMENT_MENU_DATA,
+        },
+        {
+            "name": "Menú de manejo de snapshots.",
+            "action": SNAPSHOT_MANAGEMENT_MENU_DATA,
+        },
+        {
+            "name": "Menú de obtención de información sobre USE flags.",
+            "action": USE_FLAGS_MENU_DATA,
+        },
+        {"name": "OPCIONES DE LIMPIEZA"},
+        {
+            "name": "Limpiar archivos residuales.",
+            "action": [
+                ["#ROOT", "eclean-dist", "-d"],
+                ["#ROOT", "eclean-pkg", "-d"],
+            ],
+            "aesthetic_action": "print_line",
+        },
+        {
+            "name": "Limpiar versiones antiguas del kernel.",
+            "action": [["#ROOT", "eclean-kernel", "-A", "-d", "-n 3"]],
+            "aesthetic_action": "print_line",
+        },
+        {
+            "name": "Limpiar miniaturas de Nautilus.",
+            "action": [clean_thumbnails],
+            "aesthetic_action": "print_line",
+        },
+        {"name": "MISCELÁNEA"},
+        {
+            "name": "Resolver conflictos por diferencia de archivos.",
+            "action": [["#ROOT", "dispatch-conf"]],
+            "aesthetic_action": "print_line",
+        },
+        {
+            "name": "Leer el boletín de noticias de Gentoo.",
+            "action": [read_news],
+            "aesthetic_action": "clear_screen",
+        },
+        {
+            "name": "SALIR.",
+            "action": "exit_script",
+        },
+    ],
+}
